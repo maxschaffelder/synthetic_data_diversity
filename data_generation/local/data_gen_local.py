5838d813f42f1eb6e052eaee5aa1d449a777bde8
maxschaffelder <mschaffelder@gmail.com>
2025-05-05 14:29:04 +0200

    move code for local data generation to dedicated folder

--- data_generation/local/data_gen_local.py
+++ data_generation/local/data_gen_local.py
@@ -190,8 +190,8 @@ if __name__ == "__main__":
     # Create a model-specific output path
     model_short = "Llama"
     # output_path = f"/scratch-shared/mschaffelder/Data/Finetuning/Augmented/Small/Llama_with_perplexity/dolly_train_{dolly_version}_{model_short}.jsonl"
-    output_path = f"/scratch-shared/mschaffelder/Data/Finetuning/Augmented/Small/Llama/dolly_test_{model_short}.jsonl"
-    #output_path = f"/scratch-shared/mschaffelder/Data/Finetuning/Augmented/Medium/Llama/dolly_test_{model_short}.jsonl"
+    #output_path = f"/scratch-shared/mschaffelder/Data/Finetuning/Augmented/Small/Llama/dolly_test_{model_short}.jsonl"
+    output_path = f"/scratch-shared/mschaffelder/Data/Finetuning/Augmented/Medium/Llama/dolly_test_{model_short}.jsonl"
 
 
     print(f"Generating data with {model_name}")
