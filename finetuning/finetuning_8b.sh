6f7d3de7b030adcc6a35e419dab69fa3481ad2c2
maxschaffelder <mschaffelder@gmail.com>
2025-05-09 00:25:01 +0200

    change sbatch script to run finetuning_dr

--- finetuning/finetuning_8b.sh
+++ finetuning/finetuning_8b.sh
@@ -26,4 +26,4 @@ echo "CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"
 echo "Available GPUs: $(nvidia-smi -L)"
 
 huggingface-cli login --token "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"
-python finetuning_local_8b_v2.py
+python finetuning_dr.py
