c3435157401e94ad8b925225a9c483559b1e6725
maxschaffelder <mschaffelder@gmail.com>
2025-05-22 16:01:26 +0200

    create new venv for experiment 1

--- exp_1/data_gen.sh
+++ exp_1/data_gen.sh
@@ -5,15 +5,31 @@
 #SBATCH --cpus-per-task 4
 #SBATCH --mem=20G
 #SBATCH --gpus=1
-#SBATCH --partition=gpu_a100
+#SBATCH --partition=gpu_h100
 #SBATCH -N 1
 
-# Load required modules 
+
+VENV_NAME="venv_exp_1"
+VENV_BASE_DIR="/scratch-shared/mschaffelder" # Or any other persistent shared directory you prefer
+VENV_DIR="$VENV_BASE_DIR/$VENV_NAME"
+PYTHON_MODULE="Python/3.10.4-GCCcore-11.3.0"
+
+# Load required modules
 module load 2024
-# module load Python/3.10.4-GCCcore-11.3.0
+module load $PYTHON_MODULE
+
+# Create virtual environment if it doesn't exist
+if [ ! -d "$VENV_DIR/bin" ]; then
+    echo "Creating Python virtual environment $VENV_NAME at $VENV_DIR using $(python --version)"
+    python -m venv $VENV_DIR
+else
+    echo "Virtual environment $VENV_NAME already exists at $VENV_DIR."
+fi
 
 # Activate virtual environment
-source /scratch-shared/mschaffelder/venv/bin/activate
+# source /scratch-shared/mschaffelder/venv/bin/activate
+echo "Activating virtual environment: $VENV_DIR"
+source $VENV_DIR/bin/activate
 
 # Install required packages
 pip install --upgrade pip
