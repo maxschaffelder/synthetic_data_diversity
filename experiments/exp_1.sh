8f7a1ad0ee7f22fd237bd69cec1c355e09407999
maxschaffelder <mschaffelder@gmail.com>
2025-05-22 15:25:07 +0200

    test

--- experiments/exp_1.sh
+++ experiments/exp_1.sh
@@ -26,6 +26,5 @@ cd $SLURM_SUBMIT_DIR
 echo "CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"
 echo "Available GPUs: $(nvidia-smi -L)"
 
-print("hi")
 huggingface-cli login --token "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"
 python exp_1.py
