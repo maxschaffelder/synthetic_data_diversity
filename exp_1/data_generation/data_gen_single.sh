58e19df7b69f660b04f530b6de70eb3c9112ea85
maxschaffelder <mschaffelder@gmail.com>
2025-05-30 16:25:22 +0200

    fix data paths

--- exp_1/data_generation/data_gen_single.sh
+++ exp_1/data_generation/data_gen_single.sh
@@ -58,7 +58,7 @@ fi
 
 # --- Script Configuration ---
 LORA_MODEL_PATH="/scratch-shared/mschaffelder/data/ft_models/small/lora_llama_8b_single_v8"
-TEST_DATA_PATH="/Users/maxschaffelder/Desktop/Thesis/data/finetuning/dolly/dolly_test.jsonl"
+TEST_DATA_PATH="/scratch-shared/mschaffelder/data/finetuning/dolly/dolly_test.jsonl"
 OUTPUT_DIR="/scratch-shared/mschaffelder/data/exp_1/outputs"
 # --- End Script Configuration ---
 
