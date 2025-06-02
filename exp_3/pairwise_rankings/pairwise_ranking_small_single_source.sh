b65c5ea8cb7a928f32522d2abd8520f7c1ff366d
maxschaffelder <mschaffelder@gmail.com>
2025-06-02 17:24:07 +0200

    correct input file paths

--- exp_3/pairwise_rankings/pairwise_ranking_small_single_source.sh
+++ exp_3/pairwise_rankings/pairwise_ranking_small_single_source.sh
@@ -59,7 +59,7 @@ python /scratch-shared/mschaffelder/code/exp_3/pairwise_rankings/pairwise_rankin
     --base_model_path "meta-llama/Llama-3.1-8B-Instruct" \
     --lora_model_path "/scratch-shared/mschaffelder/data/ft_models/small/lora_llama_8b_single_v8" \
     --use_lora \
-    --input_file "/scratch-shared/mschaffelder/data/exp_3/pairwise_data/pairwise_ranking_input_small_ab.jsonl" \
+    --input_file "/scratch-shared/mschaffelder/data/exp_3/pairwise_data/pairwise_ranking_input_small_a_b.jsonl" \
     --output_file "/scratch-shared/mschaffelder/data/exp_3/pairwise_rankings/a_b/small_single_source.jsonl" \
     --ranking_markers "a b" \
     --system_prompt "You are a helpful assistant. Your task is to rank the two provided texts. Please explicitly write which of the two texts is of higher quality by writing the corresponding letter in the output, and nothing else."
--- exp_3/pairwise_rankings/pairwise_ranking_small_vanilla.sh
+++ exp_3/pairwise_rankings/pairwise_ranking_small_vanilla.sh
@@ -57,7 +57,7 @@ fi
 
 python /scratch-shared/mschaffelder/code/exp_3/pairwise_rankings/pairwise_ranking.py \
     --base_model_path "meta-llama/Llama-3.1-8B-Instruct" \
-    --input_file "/scratch-shared/mschaffelder/data/exp_3/pairwise_data/pairwise_ranking_input_small_ab.jsonl" \
+    --input_file "/scratch-shared/mschaffelder/data/exp_3/pairwise_data/pairwise_ranking_input_small_a_b.jsonl" \
     --output_file "/scratch-shared/mschaffelder/data/exp_3/pairwise_rankings/a_b/small_vanilla.jsonl" \
     --ranking_markers "a b" \
     --system_prompt "You are a helpful assistant. Your task is to rank the two provided texts. Please explicitly write which of the two texts is of higher quality by writing the corresponding letter in the output, and nothing else."
