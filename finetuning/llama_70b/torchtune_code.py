63958613930c502f558ebb4799a2d017af761702
maxschaffelder <mschaffelder@gmail.com>
2025-06-06 16:39:38 +0200

    change import name

--- finetuning/llama_70b/torchtune_code.py
+++ finetuning/llama_70b/torchtune_code.py
@@ -25,7 +25,7 @@ from torchtune import config, modules, training, utils
 from torchtune.config._utils import _get_component_from_path
 from torchtune.data import padded_collate_packed
 from torchtune.datasets import ConcatDataset
-from torchtune.modules.loss import SFTLoss
+from torchtune.modules.loss import LinearCrossEntropyLoss
 from torchtune.modules.peft import (
     AdapterModule,
     get_adapter_params,
@@ -339,7 +339,7 @@ class LoRAFinetuneRecipeDistributed(FTRecipeInterface):
 
         # initialize loss
         self._loss_fn = config.instantiate(cfg.loss)
-        if isinstance(self._loss_fn, SFTLoss):
+        if isinstance(self._loss_fn, LinearCrossEntropyLoss):
             self._loss_fn.set_model_output(self._model)
 
         if self._compile:
@@ -812,7 +812,7 @@ class LoRAFinetuneRecipeDistributed(FTRecipeInterface):
             outputs = self._model(**batch)
 
         # post process for third party loss functions
-        if not isinstance(self._loss_fn, SFTLoss):
+        if not isinstance(self._loss_fn, LinearCrossEntropyLoss):
             labels = labels.reshape(-1)
             outputs = outputs.reshape(-1, outputs.size(-1))
             if isinstance(outputs, DTensor):
